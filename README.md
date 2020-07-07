d732ce07fd511a295c3669b1acfb4ae63b171cb8
seaky <seaky.cn@gmail.com>
2020-07-07 15:00:39 +0800

    0.1

--- README.md
+++ README.md
@@ -1,2 +1,118 @@
-# zabbix_monitor_port_flow
-monitor linux port flow by zabbix
+
+## Description
+Monitor designated packets/bytes of items in iptables by zabbix, such as tcp/udp port flow.
+
+Discovery items need be monitored automatically.
+
+
+
+## Depoly
+
+### Client
+
+#### Install the script
+
+`sudo cp zabbix_iptables.py /usr/bin/`
+`sudo chmod a+x /usr/bin/zabbix_iptables.py`
+
+
+
+#### Grant privilege 
+
+The script will be run as user **zabbix**, and invokes command 'iptables' which requires **root privilege**,  There are some ways to privilege non-root users.
+
+a. Set SUID to iptables
+
+`sudo chmod 4744 /sbin/iptables`
+
+OR
+
+b. Grant sudo privilege to user **zabbix**  without password by edit /etc/sudoers
+
+`zabbix    ALL=(ALL:ALL) NOPASSWD: ALL`
+
+ Then add sudo prefix to zabbix_agent config.
+
+`UserParameter=iptables.items[*],sudo /usr/bin/zabbix_iptables.py`
+
+
+
+#### Add config in zabbix_agent
+
+`sudo vi /etc/zabbix/zabbix_agentd.conf.d/zabbix_iptables.conf`
+
+```
+UserParameter=iptables.items[*],/usr/bin/zabbix_iptables.py
+UserParameter=iptables.count[*],/usr/bin/zabbix_iptables.py --unit $1 --table $2 --chain $3 --comment $4
+UserParameter=iptables.count.auto[*],/usr/bin/zabbix_iptables.py --unit $1 --key $2
+```
+
+| api                 | function                |
+| ------------------- | ----------------------- |
+| iptables.items      | discovery items         |
+| iptables.count      | get data by designation |
+| iptables.count.auto | get data by discovery   |
+
+restart zabbix_agent
+
+`sudo /etc/init.d/zabbix-agent restart` 
+
+#### Modify IPTABLES rules
+
+By default, rule will discovery items with comment starts with **ZABBIX**, and name this items by their comment.
+
+For example, the record below will be assign key as **FILTER_INPUT_SSH_IN** on zabbix server.
+
+```
+Chain INPUT (policy ACCEPT 791 packets, 55660 bytes)
+num   pkts bytes target     prot opt in     out     source               destination
+1    16764 1446K            tcp  --  *      *       0.0.0.0/0            0.0.0.0/0            tcp dpt:22222 /* ZABBIX SSH_IN */
+```
+
+![1](img/1.png)
+
+
+
+### Server 	
+
+#### Test client
+
+![7](E:\mygit\zabbix_iptables\img\7.png)
+
+#### Add discovery rules
+
+![](img/2.png)
+
+#### Create rule
+
+key = iptables.items
+
+![3](img/3.png)
+
+#### Create item prototypes
+
+name = IPTABLE_{#IPTBS_ITEM}
+
+key = 	iptables.count.auto[bytes,{#IPTBS_ITEM}]
+
+Store value = Delta
+
+![4](img/4.png)
+
+
+
+## Display
+
+![](img/5.png)
+
+
+
+![](E:\mygit\zabbix_iptables\img\6.png)
+
+
+
+## Other
+
+if config as below, it will discovery all item whether it has comment.
+
+`UserParameter=iptables.items[*],/usr/bin/zabbix_iptables.py --no_comment`
---
+++ zabbix_iptables.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Seaky
+# @Date:   2020/7/6 19:02
+
+
+import argparse
+import json
+import re
+from subprocess import run, PIPE, STDOUT
+
+BASE = 1024
+
+
+def execute(cmd, stdout=PIPE, stderr=STDOUT, encoding='utf-8', shell=False, *args, **kwargs):
+    if isinstance(cmd, str):
+        shell = True
+    p = run(cmd, stdout=stdout, stderr=stderr, shell=shell, *args, **kwargs)
+    if isinstance(p.stdout, bytes):
+        p.stdout = p.stdout.decode(encoding)
+    if isinstance(p.stderr, bytes):
+        p.stderr = p.stderr.decode(encoding)
+    return p
+
+
+def convert(s):
+    l = ['k', 'm', 'g', 't']
+    if s.isdigit():
+        return s
+    else:
+        for i, c in enumerate(l):
+            if s[-1].lower() == c:
+                return str(int(s[:-1]) * BASE ** (i + 1))
+
+
+def get_items():
+    data = []
+    p1 = re.compile('Chain (?P<chain>\w+) \(policy')
+    p2 = re.compile('^(?P<id>\d+)\s+(?P<pkts>\w+)\s+(?P<bytes>\w+)')
+    p3 = re.compile('/\* ZABBIX (?P<comment>.*) \*/')
+
+    for table in ['filter', 'nat']:
+        p = execute('/sbin/iptables -t {} -nvL --line-numbers'.format(table))
+
+        chain = ''
+        for line in p.stdout.split('\n'):
+            m1 = p1.search(line)
+            if m1:
+                chain = m1.group('chain')
+                continue
+
+            m2 = p2.search(line)
+            if m2:
+                d = m2.groupdict()
+                d['chain'] = chain
+                for x in ['pkts', 'bytes']:
+                    d[x] = convert(d[x])
+                m3 = p3.search(line)
+                if m3:
+                    d['comment'] = m3.group('comment').strip()
+                    d['name_type'] = 'comment'
+                if not d.get('comment'):
+                    d['comment'] = '{id}'.format(**d)
+                    d['name_type'] = 'id'
+                d['table'] = table.upper()
+                d['name'] = '{table}_{chain}_{comment}'.format(**d)
+                data.append(d)
+    return data
+
+
+def parse(key=None, unit='', table='', chain='', comment='', no_comment=False):
+    items = get_items()
+    if key:
+        table, chain, *_ = key.split('_')
+        comment = '_'.join(_)
+    if not unit:
+        return json.dumps(
+            {'data': [{'{#IPTBS_ITEM}': x['name']} for x in items if no_comment or x['name_type'] == 'comment']})
+    else:
+        for x in items:
+            if table.upper() == x['table'] and chain.upper() == x['chain'] and \
+                    (comment.upper() == x['comment'].upper() or comment == x['id']):
+                return x.get(unit)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--unit', default='', help='pkts/bytes')
+    parser.add_argument('--table', default='', help='filter/nat')
+    parser.add_argument('--chain', default='', help='input/output/forward/prerouting/postrouting')
+    parser.add_argument('--comment', default='', help='comment/chain_id')
+    parser.add_argument('--key', default='', help='zabbix discover key')
+    parser.add_argument('--no_comment', action='store_true', help='discover items without "ZABBIX" in comment')
+    args = parser.parse_args()
+
+    print(parse(**args.__dict__))
